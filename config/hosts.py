105867ae005a2fa6688b9c3e57269539841db5cb
atiemoasare <asarebernard98@gmail.com>
2026-03-10 00:15:45 -0400

    Remove 'api' subdomain prefix from host pattern to use root domain

--- config/hosts.py
+++ config/hosts.py
@@ -1,7 +1,7 @@
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host(r'api', 'config.urls.api', name='api'),
+    host(r'', 'config.urls.api', name='api'),
     host(r'www', 'config.urls', name='www'),
     host(r'', 'config.urls', name='root'),
 )
